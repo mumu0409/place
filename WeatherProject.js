400d18c301906ccf139957e53cebf22c808c68d8
root <jak10504@naver.com>
2020-12-24 14:33:46 +0000

    qo

---
+++ WeatherProject.js
@@ -0,0 +1,39 @@
+import React from 'react';
+import { StyleSheet, Text, View, TextInput, Image } from 'react-native';
+
+import Forecast from "./Forecast";
+import OpenWratherMap from "./open_weathe_map";
+
+class WeatherProject extends Component {
+	constructor(props) {
+		super(props);
+		this.state = {zip:"",forecast:null}
+	}
+	
+	_handleTextChange = event => {
+		let zip = event.nativeEvent.text;
+		OpenWeatherMap.fetchForecast(zip).then(forecast => {
+			this.setState({forecast : forecast}) ;
+		});
+	};
+
+render() {
+	let content = null;
+	if(this.state.forecast != null){
+		content = (
+		<Forecast
+			main={this.state.forecast.main}
+			description={this.state.forecast.description}
+			temp={this.state.forecast.temp}
+			></Forecast>
+		);
+	}
+	
+	return (
+		<View>
+			<Image 
+				source={require()
+		</View>
+	)
+}
+}
---
+++ component.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+<Button
+	OnPress={this._props}
+	title="Press"
+	color="#841584"
+	accessibilityLabel="Press Button"
+	
+/>
+									 
